99580b241fd5cc8a0028b1abf438d164181607a2
no5ix <1145263208@qq.com>
2023-10-19 00:21:05 +0800

    update auto-toc: 1. 缩小图片 2. anti-jump-warning 3. 谷歌禁止重定向 4. 知乎-向下翻时自动隐藏顶栏

--- auto_toc.js
+++ auto_toc.js
@@ -2,7 +2,7 @@
 // @name         auto-toc
 // @name:zh-CN   auto-toc
 // @namespace    EX
-// @version      1.12
+// @version      1.13
 // @license MIT
 // @description Generate table of contents for any website. By default, it is not open. You need to go to the plug-in menu to open the switch for the website that wants to open the toc. The plug-in will remember this switch, and the toc will be generated automatically according to the switch when you open the website the next time.
 // @description:zh-cn 可以为任何网站生成TOC网站目录大纲, 默认是不打开的, 需要去插件菜单里为想要打开 toc 的网站开启开关, 插件会记住这个开关, 下回再打开这个网站会自动根据开关来生成 toc 与否. 高级技巧: 单击TOC拖动栏可以自动折叠 TOC, 双击TOC拖动栏可以关闭 TOC .
@@ -4401,6 +4401,70 @@
         }
     }
 
+    //////////////////////////////////////// 所有网站-缩小图片
+    function shrink_img(from_menu_switch=false) {
+        var domain2shouldShrinkImg = GM_getValue("menu_GAEEScript_shrink_img");
+        var shouldShrinkImg = domain2shouldShrinkImg[window.location.host];
+        console.log(
+            "[shrink_img]"
+        );
+        let shouldNotShrink = shouldShrinkImg == null || !shouldShrinkImg
+        if (!from_menu_switch && shouldNotShrink) {
+            return;
+        }
+        let cssTxt = '';
+        Array.from(document.getElementsByTagName('*')).forEach(ele=>{
+            if (ele.tagName === 'IMG') {
+                // if (['.png', '.jpg', '.gif'].some(i=>location.href.includes(i)))
+                //     return;
+                // //忽略图片预览类网址
+                // const imgWidthStr = getComputedStyle(ele).width;
+                // const imgWidthRes = imgWidthStr.match(/^([+-]?(?:\d*\.\d+|\d+\.?)(?:[eE][+-]?\d+)?)\s*([a-zA-Z]*)$/);
+                // if (imgWidthRes) {
+                //     const num = imgWidthRes.at(1);
+                //     const unit = imgWidthRes.at(2);
+                //     const isTooBig = unit === 'rem' ? Number(num) > 10 : Number(num) > 500
+                //     if (isTooBig) {
+                        //hover时恢复图片尺寸
+                        // const noAnimation = ['all 0s ease 0s', "none 0s ease 0s"].includes(getComputedStyle(ele).transition);
+
+                        // if (noAnimation) {
+                            const genCSSSelector = (ele)=>{
+                                if (ele.id)
+                                    return `img[id="${ele.id}"]:hover`
+                                else {
+                                    if(ele.src.startsWith('data:')) return `img[src="${ele.src}"]`;//base64的src
+                                    else{
+                                        const the_src = ele.src || ele.getAttribute('_src')
+|| '找不到可用选择器';
+                                        //http的src
+                                        const url = new URL(the_src)//_src是一些网站懒加载的
+                                        return `img[src="${url.pathname + url.search}"]:hover,img[src="${the_src}"]:hover`;
+                                    }
+                                }
+                            }
+                            cssTxt += `${genCSSSelector(ele)}{` +
+                                (shouldNotShrink ? "" : `width:${ele.width}px !important;height:${ele.height}px !important;`) +
+                             `}`;
+                        // }
+
+                        //变小
+                        ele.style.width = shouldNotShrink ? 'auto' : "150px";
+                        ele.style.height = 'auto';
+                    // }
+
+                    //使用
+
+                // }
+            }
+
+        }
+        )
+
+        insertCSS(cssTxt, "shrinkimg__css");
+        console.log('缩小图片完成');
+    }
+
     var menu_ALL = [
             [
                 "menu_GAEEScript_auto_open_toc",
@@ -4410,6 +4474,11 @@
             [
                 "menu_GAEEScript_auto_collapse_toc",
                 "Collapse TOC on current site(当前网站TOC自动折叠开关)",
+                {},
+            ],
+            [
+                "menu_GAEEScript_shrink_img",
+                "Shrink IMG on current site(当前网站缩小图片开关)",
                 {},
             ],
         ],
@@ -4505,9 +4574,8 @@
                 domain2offset
             );
             GM_setValue("menu_GAEEScript_auto_collapse_toc", domain2isCollapse);
-        } else if (
-            localStorageKeyName === "menu_GAEEScript_auto_collapse_toc"
-        ) {
+            handleToc();
+        } else if (localStorageKeyName === "menu_GAEEScript_auto_collapse_toc") {
             console.log(
                 "[menuSwitch menu_GAEEScript_auto_collapse_toc]",
                 domain2isCollapse
@@ -4521,6 +4589,23 @@
                 toast("Turn Off TOC Auto Collapse.");
             }
             GM_setValue(`${localStorageKeyName}`, domain2isCollapse);
+            handleToc();
+        } else if (localStorageKeyName === "menu_GAEEScript_shrink_img") {
+            var domain2shouldShrinkImg = GM_getValue("menu_GAEEScript_shrink_img");
+            console.log(
+                "[menuSwitch menu_GAEEScript_shrink_img]",
+                domain2shouldShrinkImg
+            );
+            var shouldShrinkImg = domain2shouldShrinkImg[window.location.host];
+            if (shouldShrinkImg == null || !shouldShrinkImg) {
+                domain2shouldShrinkImg[window.location.host] = true;
+                toast("Turn On Shrink IMG.");
+            } else {
+                delete domain2shouldShrinkImg[window.location.host];
+                toast("Turn Off Shrink IMG.");
+            }
+            GM_setValue(`${localStorageKeyName}`, domain2shouldShrinkImg);
+            shrink_img(true);
         }
         // if((/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent))) {
         //     alert("这是safari")
@@ -4535,7 +4620,6 @@
             // alert("非safari");
             registerMenuCommand(); // 重新注册脚本菜单
         }
-        handleToc();
         // location.reload(); // 刷新网页
     }
 
@@ -4585,7 +4669,9 @@
         }
         handleToc();
 
-        if (window.location.host == "www.zhihu.com") {
+        const urlObj = new URL(window.location.href);
+        if (urlObj.host === "www.zhihu.com") {
+            //////////////////////////////////////// 知乎-向下翻时自动隐藏顶栏
             let style = "";
             let style_3 = `/* 向下翻时自动隐藏顶栏*/
                 header.is-hidden {display: none;}
@@ -4606,9 +4692,64 @@
                     }
                 });
             }
+        } else if (urlObj.host === "www.google.com") {
+            //////////////////////////////////////// google-禁止重定向
+            var url = window.location.href.toLowerCase();
+            if (url.indexOf("/search") >= 0 || url.indexOf("/url") >= 0) {
+                function clean() {
+                    // 获取id为"center_col"的div元素
+                    const centerCol = document.getElementById('center_col');
+
+                    // 获取所有超链接
+                    var links = centerCol.getElementsByTagName('a');
+
+                    // 遍历超链接并移除不必要的属性
+                    let url
+                    for (let i = 0; i < links.length; i++) {
+                        const link = links[i];
+                        // 移除不必要的属性
+                        if (link.hasAttribute('button')) {
+                            continue;
+                            //alert("found!");
+                        }
+                        url = links[i].getAttribute('href');
+                        var match = /url=(.*?)&/.exec(url);
+                        if (match) {
+                            links[i].setAttribute('href', decodeURIComponent(match[1]));
+                        }
+                        link.removeAttribute('data-jsarwt');
+                    }
+                }
+                setTimeout(clean, 10);
+                setTimeout(clean, 500);
+                for (let i = 1; i <= 240; i++) {
+                    setTimeout(clean, 1000 * i);
+                }
+            }
         }
-        // setTimeout(function() {
-        //     handleToc();
-        // }, 2800);
+
+        //////////////////////////////////////// 所有网站-缩小图片
+        shrink_img();
+        setTimeout(shrink_img, 1888);
+
+        //////////////////////////////////////// 所有网站-anti-jump-warning
+        const parameters = {
+            'link.zhihu.com': 'target',
+            'link.csdn.net': 'target',
+            'link.juejin.cn': 'target',
+            'www.jianshu.com': 'url',
+            'www.oschina.net': 'url',
+            'gitee.com': 'target',
+            'weibo.cn': 'u',
+        };
+        const selectors = {
+            'jump2.bdimg.com': 'body > div > div.warning_info > p.link',
+        };
+        if (parameters[urlObj.host]) {
+            location.replace(urlObj.searchParams.get(parameters[urlObj.host]));
+        } else if (selectors[urlObj.host]) {
+            location.replace(document.querySelector(selectors[urlObj.host]).innerHTML);
+        }
+
     }
 })();
