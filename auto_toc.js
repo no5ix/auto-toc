fd102112f276c22f633226aac362de1ce1beedec
no5ix <no5ix@foxmail.com>
2023-10-31 19:50:17 +0800

    update: scroll spped

--- auto_toc.js
+++ auto_toc.js
@@ -2,7 +2,7 @@
 // @name         auto-toc
 // @name:zh-CN   auto-toc
 // @namespace    EX
-// @version      1.26
+// @version      1.27
 // @license MIT
 // @description Generate table of contents for any website. By default, it is not open. You need to go to the plug-in menu to open the switch for the website that wants to open the toc. The plug-in will remember this switch, and the toc will be generated automatically according to the switch when you open the website the next time.
 // @description:zh-cn 可以为任何网站生成TOC网站目录大纲, 默认是不打开的, 需要去插件菜单里为想要打开 toc 的网站开启开关, 插件会记住这个开关, 下回再打开这个网站会自动根据开关来生成 toc 与否. 高级技巧: 单击TOC拖动栏可以自动折叠 TOC, 双击TOC拖动栏可以关闭 TOC .
@@ -3656,7 +3656,7 @@
             targetElem: node,
             scrollElem: scrollElem,
             topMargin: topMargin,
-            maxDuration: 188,
+            maxDuration: 566,
             callback: onScrollEnd && onScrollEnd.bind(null, node),
         });
     };
@@ -4386,7 +4386,7 @@
                         // console.log(node);
                         continue;
                     }
-                    // 加粗的文字的父元素带下划线不识别为标题
+                    // 加粗的文字的父元素为<u>则不识别为标题(因为<u>会使得子元素带下划线)
                     if (node.parentElement.tagName == "U") {
                         // console.log("b_strong continue 5");
                         // console.log(node);
