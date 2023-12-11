f770da5402ebbf4ad0a94bb613289c0320ac8a61
no5ix <1145263208@qq.com>
2023-12-11 21:33:18 +0800

    modify css: lower auto-toc z-index

--- auto_toc.js
+++ auto_toc.js
@@ -2,7 +2,7 @@
 // @name         auto-toc
 // @name:zh-CN   auto-toc
 // @namespace    EX
-// @version      1.45
+// @version      1.46
 // @license MIT
 // @description Generate table of contents for any website. By default, it is not open. You need to go to the plug-in menu to open the switch for the website that wants to open the toc. The plug-in will remember this switch, and the toc will be generated automatically according to the switch when you open the website the next time.
 // @description:zh-cn 可以为任何网站生成TOC网站目录大纲, 默认是不打开的, 需要去插件菜单里为想要打开 toc 的网站开启开关, 插件会记住这个开关, 下回再打开这个网站会自动根据开关来生成 toc 与否. 高级技巧: 单击TOC拖动栏可以自动折叠 TOC, 双击TOC拖动栏可以关闭 TOC .
@@ -402,13 +402,13 @@
                 }
             
                 #smarttoc.dark-scheme a {
-                    color: #ccc;
+                    /* color: #ccc; */
                 }
             
                 #smarttoc.dark-scheme a:hover,
                 #smarttoc.dark-scheme a:active {
-                    border-left-color: #f6f6f6;
-                    color: #fff;
+                    /* border-left-color: #f6f6f6; */
+                    /* color: #fff; */
                 }
             
                 #smarttoc.dark-scheme li.active>a {
@@ -439,7 +439,7 @@
                 ? "max-height: 22px;"
                 : "max-height: calc(100vh - 100px);") +
             `
-                z-index: 10000;
+                z-index: 888;
                 box-sizing: border-box;
                 /* background-color: #fff; */
                 color: gray;
