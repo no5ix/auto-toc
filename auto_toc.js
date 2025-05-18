c39972ab569680c591a25ad8aa404a989e7346c6
no5ix <1145263208@qq.com>
2025-05-17 17:39:10 -0700

    remove a log

--- auto_toc.js
+++ auto_toc.js
@@ -2,7 +2,7 @@
 // @name         auto-toc
 // @name:zh-CN   auto-toc
 // @namespace    EX
-// @version      1.63
+// @version      1.64
 // @license MIT
 // @description Generate table of contents for any website. By default, it is not open. You need to go to the plug-in menu to open the switch for the website that wants to open the toc. The plug-in will remember this switch, and the toc will be generated automatically according to the switch when you open the website the next time.
 // @description:zh-cn 可以为任何网站生成TOC网站目录大纲, 默认是不打开的, 需要去插件菜单里为想要打开 toc 的网站开启开关, 插件会记住这个开关, 下回再打开这个网站会自动根据开关来生成 toc 与否. 高级技巧: 单击TOC拖动栏可以自动暗淡 TOC, 双击TOC拖动栏可以关闭 TOC .
@@ -4455,11 +4455,11 @@
             clearInterval(checkBrowserWidthChangeTimerId);
         }
         autoGenTocTimerId = setInterval(() => {
-            if (shouldLog)
-                console.log(
-                    "[auto_toc] - handleToc autoGenTocTimerId window.location.host=",
-                    window.location.host
-                );
+            // if (shouldLog)
+            //     console.log(
+            //         "[auto_toc] - handleToc autoGenTocTimerId window.location.host=",
+            //         window.location.host
+            //     );
             if (toc && toc.isValid()) {
                 // clearInterval(timerId); 如果不注释的话, 就会终止这个 timer 从而导致在页面未刷新但是标题改变的时候无法自动生成最新的标题
                 // return;
